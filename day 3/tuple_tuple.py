a32ed17522ac5d9b2d7becc843603b5928115297
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-01-18 12:04:51 +0530

    Create tuple_tuple.py

---
+++ day 3/tuple_tuple.py
@@ -0,0 +1,30 @@
+# list2 = [(1,2,3),('x','Y','Z','A')]
+# print (list2)
+# print (len(list2))
+# print (len(list2[1]))
+
+# tup1 =(1,2,3,'x','y','m')
+# tup2 =('x','y','m','N')
+# print (len(tup1))
+# print (max(tup2))
+# print (min(tup2))
+
+# tup1=(10,20,30)
+# tup2=tup1 [:2]
+# tup3=tup1[2:]*3
+# tup1 = tup2+tup3
+# print (tup1)
+
+# tup1 =(1,2,3)
+# tup2 =(4,5,6)
+# tup3 = tup1+tup2
+# print(tup3)
+# del(tup2)
+# # print(tup2)
+
+# a = 'Python'
+# b = 'Training'
+# print ('Welcome to %s %s' % (a,b))
+
+str1 = 'Good Day'
+print (str1.find('D'))
